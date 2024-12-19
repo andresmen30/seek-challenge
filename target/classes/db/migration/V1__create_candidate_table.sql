d4245b36e662d23294eeb744dcebacbf9912637b
Andres Mendez <andres.mendez@astropay.com>
2024-12-18 23:52:00 -0500

    removed files

--- target/classes/db/migration/V1__create_candidate_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE candidate (id INT AUTO_INCREMENT PRIMARY KEY,
-                           name VARCHAR(255) NOT NULL,
-                           email VARCHAR(255) NOT NULL UNIQUE,
-                           gender ENUM('MALE', 'FEMALE', 'OTHER') NOT NULL,
-                           salary_expected DECIMAL(10, 2) NOT NULL
-);
