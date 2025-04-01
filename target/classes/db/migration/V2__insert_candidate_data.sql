162f041f0f0e01b443f17e517cedd2c1958b47c1
Andres Mendez <andres.mendez@astropay.com>
2025-04-01 12:02:59 -0500

    clean code, files and directory

--- target/classes/db/migration/V2__insert_candidate_data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO candidate (name, email, gender, salary_expected)
-VALUES
-    ('John Doe', 'john.doe@seek.com', 'MALE', 1500.00),
-    ('Jane Smith', 'andres.mendez@seek.com', 'MALE', 3000.00),
-    ('Andres Mendez', 'alex.taylor@seek.com', 'OTHER', 2100.00),
-    ('Veronica Forero', 'vero.forero@seek.com', 'FEMALE', 2100.00),
-    ('Falcao Garcia', 'falcao.garcia@seek.com', 'MALE', 2100.00);
